f05700c397ca3841d38d00223899d83b6af971a3
Cavit Pınar <cavo045@gmail.com>
2015-11-24 23:30:28 +0200

    deneme

---
+++ Deneme/src/Tester.java
@@ -0,0 +1,22 @@
+
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	/*	boolean[][] grid = new boolean[][] {
+				{ true, true, false, false, false },
+				{ false, true, false, false, true },
+				{ false, false, false, true, true },
+				{ false, false, false, false, false },
+				{ false, false, false, false, true } };
+*/
+		System.out.println("cavit");
+		
+		
+	}
+	
+	
+	
+}
